f519e9aa2c7d587e2f469022b6ef1e0189d0e0a7
nathapon <hellonathapon@gmail.com>
2022-03-23 09:19:46 +0700

    Config body parser

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "OK bro" });
+  res.status(200).json({ message: "OK bro! :)" });
 });
 app.post("/login", async (req, res) => {
-  res.status(200).json({ message: "got ya" });
+  const { username, password } = req.body;
+  res.status(200).json({ username, password });
 });
 
 const port = process.env.PORT || 5000;
